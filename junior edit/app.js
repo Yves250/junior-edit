7aefc4023ca948b9dbcaa687a18e4dc95d5f2016
coolkid250 <103322267+coolkid250@users.noreply.github.com>
2022-05-05 22:46:19 +0200

    Add files via upload

---
+++ junior edit/app.js
@@ -0,0 +1,13 @@
+document.getElementById('red').addEventListener('click', (event) => {
+    let element_contact = document.getElementById('aboutMe');
+    element_contact.style.backgroundColor = '#ff0000';
+    element_contact.style.color = '#ffffff';
+  
+  });
+  
+  document.getElementById('blue').addEventListener('click', (event) => {
+    let element_contact2 = document.getElementById('aboutMe');
+    element_contact2.style.backgroundColor = '#3333ff';
+    element_contact2.style.color = '#ffffff';
+  
+  });
